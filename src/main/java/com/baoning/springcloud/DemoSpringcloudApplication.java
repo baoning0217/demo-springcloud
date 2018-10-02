b4ede88683e4b4099ae3f683b60c571c67ae4e3b
baoning <bnwyyx217@163.com>
2018-10-02 11:41:11 +0800

    创建springcloud工程

---
+++ src/main/java/com/baoning/springcloud/DemoSpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.baoning.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudApplication.class, args);
+	}
+}
